74ccb1eaf3ca817654024822b7263e6eaf9f0538
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2019-10-28 14:55:56 +1300

    fix serial bug

--- ARDUINO_code_LIVE/ARDUINO_code_LIVE.ino
+++ ARDUINO_code_LIVE/ARDUINO_code_LIVE.ino
@@ -81,7 +81,7 @@ void setup() {
   // initialize serial communication
   // (115200 chosen because it is required for Teapot Demo output, but it's
   // really up to you depending on your project)
-  Serial.begin(155200);
+  Serial.begin(115200);
   while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
   // initialize device
